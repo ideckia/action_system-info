271d3d73a51b019f8e08f6b6ed5234237f103bce
Josu Igoa <josuigoa@gmail.com>
2023-11-02 07:32:31 +0100

    update Action.description

--- SystemInfo.hx
+++ SystemInfo.hx
@@ -10,7 +10,7 @@ typedef Props = {
 }
 
 @:name("system-info")
-@:description("Shows the information about the computers battery")
+@:description("Shows the information about the system")
 class SystemInfo extends IdeckiaAction {
 	var updateTimer:haxe.Timer;
 
